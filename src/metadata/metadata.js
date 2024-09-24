e437503da79916e7d0f3bcdad28a11823d877f02
berylramadhian <berylramadhian@gmail.com>
2024-09-24 14:50:42 +0200

    1st commit

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -1,13 +1,14 @@
 module.exports = {
-  name: 'William Shakespeare',
-  title: 'Poet, playwright and actor',
+  name: 'Beryl Ramadhian Aribowo',
+  title: 'PhD student',
   facts: {
-    'Residence': '<a href="https://www.google.com/maps/place/London,+UK/@51.5285582,-0.2416814,11z/data=!3m1!4b1!4m5!3m4!1s0x47d8a00baf21de75:0x52963a5addd52a99!8m2!3d51.5072178!4d-0.1275862"><i class="fa fa-home fact-icon"></i>London</a>',
-    'Wiki': `<a href="https://en.wikipedia.org/wiki/William_Shakespeare">
-                  <i class="fa-brands fa-wikipedia-w fact-icon"></i>William_Shakespeare
+    'Residence': '<a href="https://maps.app.goo.gl/j7s9Nrdw6CQkXxebA"><i class="fa fa-home fact-icon"></i>Vienna</a>',
+    'Github': `<a href="https://github.com/berylgithub">
+                  <i class="fa-brands fa-github fact-icon"></i>berylgithub
                 </a>`,
-    'Twitter': `<a href="https://twitter.com/shakespeare"><i class="fa-brands fa-twitter fact-icon"></i>Shakespeare</a>`,
-    'Email': '<a href="mailto:wil64@yahoo.com"><i class="fa fa-envelope fact-icon"></i>wil64@yahoo.com</a>'
+    'Linkedin': `<a href=" https://www.linkedin.com/in/beryl-ramadhian-aribowo"><i class="fa-brands fa-linkedin fact-icon"></i>Beryl R. Aribowo</a>`,
+    'University personal page': `<a href=" https://mat.univie.ac.at/~baribowo"><i class="fa-solid fa-building-columns fact-icon"></i>baribowo</a>`,
+    'Email': '<a href="mailto:berylramadhian@gmail.com"><i class="fa fa-envelope fact-icon"></i>berylramadhian@gmail.com</a>'
   },
   skills: [
     ['Comedy', 100],
@@ -23,46 +24,60 @@ module.exports = {
     ['Literature', 100],
     ['Influence', 100],
   ],
+  educations: [
+    {
+      title: 'PhD in Mathematics',
+      period: 'December 2020 - present (estimated completion on December 2024)',
+      contents: 'University of Vienna - Vienna Graduate School of Computational Optimization (VGSCO) - Faculty of Mathematics, Vienna, Austria.',
+      topic: `Machine learning for chemical data.`
+    },
+    {
+      title: 'MSc in Computational Science (Double degree program)',
+      period: 'March 2019 - March 2020',
+      contents: 'Kanazawa University (KU) - Graduate School of Natural Science and Technology - Division of Mathematical and Physical Sciences, Kanazawa, Japan.',
+      topic: `Machine learning for molecular binding.`
+    },
+    {
+      title: 'MSc in Computational Science (Double degree program)',
+      period: 'July 2018 - August 2020',
+      contents: `Institut Teknologi Bandung (ITB) - Fakultas Matematika dan Ilmu Pengetahuan Alam, Bandung, Indonesia.`,
+      topic: `Optimization for protein folding.`
+    },
+    {
+      title: 'BSc in Informatics',
+      period: 'July 2012 - July 2016',
+      contents: `Telkom University - Fakultas Informatika, Bandung, Indonesia.`,
+      topic: `Reinforcement learning for traffic light control.`
+    },
+  ],
   positions: [
     {
-      title: 'Hamlet',
-      period: 'Likely early 17th century',
-      skills: ['tragedy', 'philosophy', 'death'],
-      contents: `
-Prince Hamlet is visited by his father's ghost and ordered to avenge his father's murder by killing King Claudius, his uncle. After struggling with several questions, including whether what the ghost said is true and whether it is right for him to take revenge, Hamlet, along with almost all the other major characters, is killed.
-`
+      title: 'Freelance Software Engineer',
+      period: 'January 2018 - August 2018',
+      skills: ['Software engineering - frontend', 'Software engineering - backend', 'C#', 'Golang'],
+      contents: `..`
     },
     {
-      title: 'Twelfth Night',
-      period: '1600 – 1601',
-      skills: ['comedy', 'love'],
-      contents: `
-Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebastian has died in the wreck, disguises herself as a man to gain a position in Duke Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
-`
-    },
-    {
-      title: 'The Tragedy of Romeo and Juliet',
-      period: '1595–1596, with a possible early draft written in 1591',
-      skills: ['tragedy', 'death', 'love'],
-      contents: `
-In Verona, Italy, two families, the Montagues and the Capulets, are in the midst of a bloody feud. Romeo, a Montague, and Juliet, a Capulet, fall in love and struggle to maintain their relationship in the face of familial hatred. After Romeo kills Juliet's cousin Tybalt in a fit of passion, things fall apart. Both lovers eventually commit suicide within minutes of each other, and the feuding families make peace over their recent grief.
-`
+      title: 'Machine Learning Engineer',
+      period: 'November 2016 - November 2017',
+      skills: ['Applied machine learning', 'Software engineering - backend', 'Python', 'Java'],
+      contents: `..`
     }
   ],
   experience: [
     {
-      header: 'Performance',
-      skills: ['theater', 'play'],
-      contents: `
-Thousands of performances of my plays have been staged since the end of the 16th century. Many of my greatest plays were performed by the Lord Chamberlain's Men and King's Men acting companies at the [Globe](https://en.wikipedia.org/wiki/Globe_Theatre) and Blackfriars Theatres.
-`
+      header: 'Research stay',
+      skills: ['Applied machine learning', 'High performance computing', 'Julia', 'Python'],
+      contents: `Research stay in the Department of Materials Science & Engineering, University of Toronto, Toronto, Canada (June 2023 - November 2023). 
+      Working in "delta machine learning for quantum chemistry" project.`
     },
     {
-      header: 'Poetry',
-      skills: ['poetry', 'erotic'],
+      header: 'Internship',
+      skills: ['Applied machine learning', 'Java'],
       contents: `
-In 1593 and 1594, when the theatres were closed because of plague, I published two narrative poems on sexual themes, Venus and Adonis and The Rape of Lucrece. I dedicated them to Henry Wriothesley, Earl of Southampton.
-`
+      Internship in GeekStudio, Cimahi (2015). 
+      Working in "Twitter account classification" project.
+      `
     }
   ]
 };
